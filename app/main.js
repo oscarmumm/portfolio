94293fe0140b61fc9c232ff9e632244a2fcf0bbb
Oscar Mumm <oscarmumm@gmail.com>
2022-11-23 00:32:47 -0300

    widget y barra inferior agregados

--- app/main.js
+++ app/main.js
@@ -1,30 +1,34 @@
-// Obtener datos de ubicación del usuario
+// Obtener datos de ubicación y clima del usuario
+const iconoClima = document.getElementById("iconoClima");
+const homeTemp = document.getElementById("homeTemp");
+const homeCiudad = document.getElementById("homeCiudad");
+
 const options = {
     enableHighAccuracy: true,
     timeout: 5000,
     maximumAge: 0
 };
 
-const  success = (pos) => {
+const ubicacionClima = (pos) => {
     const crd = pos.coords;
     console.log("location ready")
-    return ubicacion = {
-        lat: crd.latitude,
-        lon: crd.longitude
-    }
+    let lat = crd.latitude;
+    let lon = crd.longitude;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKeyClima}&units=metric`)
+    .then((response) => response.json())
+    .then((data) => {
+        homeTemp.innerText = `${Math.round(data.main.temp)}°C`;
+        iconoClima.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+        return datosClima = data;
+    })
 };
 
 const error = (err) => {
     console.warn(`ERROR(${err.code}): ${err.message}`);
 };
 
-const obtenerUbicacion = async () => {
-    try {
-        navigator.geolocation.getCurrentPosition(success, error, options);
-    } catch (error) {
-        console.log(error);
-    }
-}
+
+navigator.geolocation.getCurrentPosition(ubicacionClima, error, options);
 
 
 // Funcionalidades para el estilo Android
@@ -34,35 +38,39 @@ const circulosInicio = document.querySelectorAll(".circulo_inicio");
 
 // Datos de hora y fecha
 const date = new Date();
-const [day, month, year] = [date.getDate(), date.getMonth(), date.getFullYear()];
+const [horas, minutos, segundos] = [date.getHours(), date.getMinutes(), date.getSeconds()]
 
 // Datos del clima 
 const apiKeyClima = "1e0852abb42466a00a4d6d999c3e2b64";
 
-const obtenerClima = async () => {
-    try {
-        const fetchingClima = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${ubicacion.lat}&lon=${ubicacion.lon}&appid=${apiKeyClima}`);
-        const fetchingUbicacion = await obtenerUbicacion();
-        const datosClima = fetchingClima.json();
-        console.log(datosClima);
-    } catch (error) {
-        console.log(error);
-    }
+const divFecha = document.getElementById("divFecha");
+
+const fecha = new Date();
+const [dia, mes, anio] = [fecha.getDate(), fecha.getMonth(), fecha.getFullYear()]; 
+const actualizarFecha = (fecha) => {
+    divFecha.innerText = `${dia}-${mes}-${anio} `
+}  
+actualizarFecha()
+
+const fechaHora = () => {
+    const divReloj = document.getElementById("divReloj");
+
+    let date = new Date()
+    let hh = date.getHours()
+    let mm = date.getMinutes()
+    let ss = date.getSeconds()
+
+    hh = hh < 10 ? `0${hh}` : hh
+    mm = mm < 10 ? `0${mm}` : mm
+    ss = ss < 10 ? `0${ss}` : ss
+
+    let hora = `${hh}:${mm}:${ss}`
+    divReloj.innerText = hora
 }
 
-// function obtenerClima() {
-//     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${ubicacion.lat}&lon=${ubicacion.lon}&appid=${apiKeyClima}`)
-//     .then((response) => response.json())
-//     .then((data) => console.log(data));
-// };
+fechaHora()
+setInterval(fechaHora, 1000)
 
-const fechaClima = document.getElementById("fechaClima"); 
-
-function actualizarDatosFechaClima () {
-    fechaClima.innerText = `${day}-${month}-${year}`;
-};
-
-actualizarDatosFechaClima();
 
 botonEntrarAndroid.addEventListener("click", () => {
     circulosInicio.forEach((element)=> {
